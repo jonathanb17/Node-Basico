ce51c4fa09b42cdd2b88dbfad903543dda063795
Joni <bustamantejonathanandres@gmail.com>
2020-11-26 20:45:16 -0300

    cambios

--- calculadora.js
+++
@@ -1,5 +0,0 @@
-const matematica =require('./matematicas');
-
-console.log(matematica.suma(1,2));
-console.log(matematica.resta(5,2));
-
--- helloworld.js
+++
@@ -1,6 +0,0 @@
-//quedo en le minuo 
-
-console.log("hello world");
-
-const os = require('os');
-console.log(os.platform()); //te da la arquitectura del so
